0f82806b3782fce874db18425779dd448c764158
shaehamm <shaehamm@gmail.com>
2020-07-10 10:24:04 -0500

    Changed DB table capitalization

--- dnddb.sql
+++ dnddb.sql
@@ -2,27 +2,27 @@ drop database if exists dnddb;
 create database dnddb;
 use dnddb;
 
-create table `user` (
+create table `User` (
 	id int primary key auto_increment,
     username varchar(30) not null unique,
     `password` varchar(100) not null,
     enabled boolean not null
 );
 
-create table `role` (
+create table `Role` (
 	id int primary key auto_increment,
 	`role` varchar(30) not null
 );
 
-create table user_role (
+create table User_Role (
 	userId int not null,
     roleId int not null,
     primary key (userId, roleId),
-    foreign key (userId) references user(id),
-    foreign key (roleId) references role(id)
+    foreign key (userId) references `User`(id),
+    foreign key (roleId) references `Role`(id)
 );
 
-create table characterClass (
+create table CharacterClass (
 	id int primary key auto_increment,
     `name` varchar(20) not null,
     hitDie varchar(3) not null,
@@ -30,46 +30,46 @@ create table characterClass (
     saves varchar(30) not null
     );
     
-create table item (
+create table Item (
 	id int primary key auto_increment,
     `name` varchar(20) not null,
     `description` varchar(100)
     );
     
-create table skill (
+create table Skill (
 	id int primary key auto_increment,
     `name` varchar(30) not null,
     `description` varchar(100) not null
     );
     
-create table attackOrSpell (
+create table AttackOrSpell (
 	id int primary key auto_increment,
     `name` varchar(30) not null,
     `description` varchar(30) not null,
     damage int not null
     );
 
-create table race (
+create table Race (
 	id int primary key auto_increment,
     `name` varchar(30) not null,
     `description` varchar(100) not null
     );
     
-create table alignment (
+create table Alignment (
 	id int primary key auto_increment,
     `name` varchar(20) not null
     );
     
-create table dndCampaign (
+create table DndCampaign (
 	id int primary key auto_increment,
     map varchar(100) not null,
     `description` varchar(200) not null,
     userId int not null,
     foreign key (userId)
-		references `user`(id)
+		references `User`(id)
     );
    
-create table dndCharacter (
+create table DndCharacter (
 	id int primary key auto_increment,
     characterName varchar(30) not null,
     playerName varchar(30) not null,
@@ -96,60 +96,60 @@ create table dndCharacter (
     armorClass int not null,
     campaignId int not null,
     foreign key (campaignId)
-		references dndCampaign(id),
+		references DndCampaign(id),
     foreign key (classId)
-		references characterClass(id),
+		references CharacterClass(id),
 	foreign key (alignmentId)
-		references alignment(id),
+		references Alignment(id),
 	foreign key (raceId)
-		references race(id)
+		references Race(id)
     );
  
-create table character_item (
+create table Character_Item (
 	characterId int not null,
     itemId int not null,
     primary key(characterId, itemId),
     foreign key (characterId)
-		references dndCharacter(id),
+		references DndCharacter(id),
 	foreign key (itemId)
-		references item(id)
+		references Item(id)
 	);
     
-create table character_skill (
+create table Character_Skill (
 	characterId int not null,
     skillId int not null,
     primary key(characterId, skillId),
     foreign key (characterId)
-		references dndCharacter(id),
+		references DndCharacter(id),
 	foreign key (skillId)
-		references skill(id)
+		references Skill(id)
 	);
     
-create table character_attackOrSpell (
+create table Character_AttackOrSpell (
 	characterId int not null,
     attackOrSpellId int not null,
     primary key(characterId, attackOrSpellId),
     foreign key (characterId)
-		references dndCharacter(id),
+		references DndCharacter(id),
 	foreign key (attackOrSpellId)
-		references attackOrSpell(id)
+		references AttackOrSpell(id)
 	);
     
-insert into `user` (username, `password`, enabled) values
+insert into `User` (username, `password`, enabled) values
 	('Admin', 'password', 1),
     ('DM1', 'password', 1),
     ('Player1', 'password', 1);
     
-insert into `role` (`role`) values
+insert into `Role` (`role`) values
 	('ROLE_ADMIN'),
     ('ROLE_DM'),
     ('ROLE_PLAYER');
     
-insert into user_role (userId, roleId) values
+insert into User_Role (userId, roleId) values
 	(1, 1),
     (2, 2),
     (3, 3);
     
-UPDATE user SET password = '$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS' WHERE id = 1;
-UPDATE user SET password = '$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS' WHERE id = 2;
-UPDATE user SET password = '$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS' WHERE id = 3;
+UPDATE `User` SET `password` = '$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS' WHERE id = 1;
+UPDATE `User` SET `password` = '$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS' WHERE id = 2;
+UPDATE `User` SET `password` = '$2a$10$S8nFUMB8YIEioeWyap24/ucX.dC6v9tXCbpHjJVQUkrXlrH1VLaAS' WHERE id = 3;
