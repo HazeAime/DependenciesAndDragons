8916d891b95cb492a81646906c8e8b9de8ee52fd
shaehamm <shaehamm@gmail.com>
2020-07-14 11:26:34 -0500

    Added user to admin controller and added html for user

--- dnd/src/main/java/com/dependencies/dragons/dnd/controllers/AdminController.java
+++ dnd/src/main/java/com/dependencies/dragons/dnd/controllers/AdminController.java
@@ -5,7 +5,11 @@
  */
 package com.dependencies.dragons.dnd.controllers;
 
+import com.dependencies.dragons.dnd.repositories.RoleRepository;
+import com.dependencies.dragons.dnd.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -14,10 +18,19 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class AdminController {
+    
+    @Autowired
+    UserRepository user;
  
     @GetMapping("/admin")
     public String displayAdminScreen() {
         return "admin";
+    }
+    
+    @GetMapping("/users")
+    public String displayUsers(Model model) {
+        model.addAttribute("users", user.findAll());
+        return "users";
     }
     
 }
