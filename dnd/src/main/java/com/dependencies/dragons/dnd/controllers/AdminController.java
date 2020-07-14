07c821cc2a03925aead02224c7c24a44360728b8
benko024 <bkb@benko.com>
2020-07-14 12:57:21 -0500

    Added approve campaign mapping

--- dnd/src/main/java/com/dependencies/dragons/dnd/controllers/AdminController.java
+++ dnd/src/main/java/com/dependencies/dragons/dnd/controllers/AdminController.java
@@ -5,12 +5,17 @@
  */
 package com.dependencies.dragons.dnd.controllers;
 
+import com.dependencies.dragons.dnd.entities.DndCampaign;
+import com.dependencies.dragons.dnd.entities.DndCharacter;
+import com.dependencies.dragons.dnd.repositories.DndCampaignRepository;
 import com.dependencies.dragons.dnd.repositories.RoleRepository;
 import com.dependencies.dragons.dnd.repositories.UserRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -18,19 +23,34 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class AdminController {
-    
+
     @Autowired
     UserRepository user;
- 
+
+    @Autowired
+    DndCampaignRepository campaign;
+
     @GetMapping("/admin")
     public String displayAdminScreen() {
         return "admin";
     }
-    
+
     @GetMapping("/users")
     public String displayUsers(Model model) {
         model.addAttribute("users", user.findAll());
         return "users";
     }
-    
+
+    @GetMapping("approvecampaign")
+    public String displayCharacterApproval(Model model) {
+        model.addAttribute("campaigns", campaign.findAll());
+        return "approvecharacter";
+    }
+
+    @PostMapping("approvecampaign")
+    public String updateApproval(List<DndCampaign> campaigns) {
+        campaign.saveAll(campaigns);
+        return "redirect:/approvecharacter";
+    }
+
 }
